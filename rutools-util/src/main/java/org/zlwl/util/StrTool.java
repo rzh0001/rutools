3aa7b10d8df73458a126d285a7951870d667479b
ruanzh <rzh0001@qq.com>
2022-02-11 19:34:12 +0800

    fix

--- rutools-util/src/main/java/org/zlwl/util/StrTool.java
+++ rutools-util/src/main/java/org/zlwl/util/StrTool.java
@@ -1,4 +1,9 @@
 package org.zlwl.util;
 
+/**
+ * String工具类
+ *
+ * @author ruanzh.eth
+ */
 public class StrTool {
 }
---
+++ rutools-wallet/src/main/java/org/zlwl/wallet/TRX.java
@@ -0,0 +1,13 @@
+package org.zlwl.wallet;
+
+/**
+ * tron 工具类
+ *
+ * @author ruanzh.eth
+ */
+public class TRX {
+
+    void getBalance(){
+        String address = "TPv1rKjZkCXU6UyCfZJJdj2iWuWsFDKMGX";
+    }
+}
