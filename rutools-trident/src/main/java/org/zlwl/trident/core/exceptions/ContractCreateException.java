f219886099d0a63833c855abbc3506e0d6d32738
微风轻拂 <rzh0001@qq.com>
2022-06-16 13:16:54 +0800

    删掉测试的代码

--- rutools-trident/src/main/java/org/zlwl/trident/core/exceptions/ContractCreateException.java
+++
@@ -1,8 +0,0 @@
-package org.zlwl.trident.core.exceptions;
-
-public class ContractCreateException extends Exception {
-    
-    public ContractCreateException(String message) {
-        super(message);
-    }
-}
